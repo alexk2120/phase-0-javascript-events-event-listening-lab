5f94412e484e26ac9b470fdbbe7684066dcbb39c
alexander.Khatamov <alexk2120@gmail.com>
2021-10-27 21:21:32 -0400

    Phase1 Submission

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function addingEventListener() {
-    const input = document.getElementById('input');
+const input = document.getElementById('input');
     
-    function clickAlert() {
-      alert('I was clicked!');
-    }
+  function clickAlert() {
+   alert('I was clicked!');
+}
     
-    input.addEventListener('click', clickAlert()); 
-    }
+  input.addEventListener('click', clickAlert()); 
+  }
     
-    addingEventListener(clickAlert) 
+addingEventListener(clickAlert) 
+
+
